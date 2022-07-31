bddfc8d7a3c2345204ea6d1555e15ab3e6740dc2
Thiago <bliscosque@yahoo.com.br>
2022-07-31 00:39:53 -0500

    ended test application

--- mainview.h
+++ mainview.h
@@ -7,6 +7,9 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainView; }
 QT_END_NAMESPACE
 
+class QTcpServer;
+class QTcpSocket;
+
 class MainView : public QMainWindow
 {
     Q_OBJECT
@@ -15,7 +18,18 @@ public:
     MainView(QWidget *parent = nullptr);
     ~MainView();
 
+private slots:
+    void on_btnStartServer_clicked();
+
+    void on_btnStopServer_clicked();
+
 private:
     Ui::MainView *ui;
+    QTcpServer* m_server;
+    QTcpSocket* m_socket;
+    bool StartServer();
+    void StopServer();
+    void ExchangeData();
+    void EchoReadData();
 };
 #endif // MAINVIEW_H
